62a7a23294f3cc90d5f726b33b40c3ba0a5171bd
nicemarcela <58824632+nicemarcela@users.noreply.github.com>
2020-08-29 17:34:23 +0200

    bug fix

--- src/App.js
+++ src/App.js
@@ -27,40 +27,48 @@ function App() {
       </header>
 
 
-      <div className="contact-us">
-      <div id="mc_embed_signup">
+      <div className="contact-us" id="mc_embed_signup">
         <form action="https://whathecurl.us17.list-manage.com/subscribe/post?u=dcb0fe74937e55e28c5843b7a&amp;id=8fa0cc2c6e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank">
-          <h4>Make sure to get your pass</h4>
+          <div id="mc_embed_signup_scroll">
+            <h4>Make sure to get your pass</h4>
             <div className="mc-field-group">
               <label for="mce-EMAIL">
-              <input required style={{ fontFamily: "Comfortaa" }} placeholder="Email*" type="email" name="customerEmail" id="mce-EMAIL"  />
+                <input
+                  required type="email" name="EMAIL" id="mce-EMAIL" />
               </label>
-              
+
             </div>
-          <div id="mce-responses" className="clear">
-            <div className="response" id="mce-error-response" style={{display:"none"}}></div>
-            <div className="response" id="mce-success-response" style={{display:"none"}}></div>
+            <div id="mce-responses" className="clear">
+              <div className="response" id="mce-error-response" style={{ display: "none" }}></div>
+              <div className="response" id="mce-success-response" style={{ display: "none" }}></div>
+            </div>
+            <div style={{ position: "absolute", left: "-5000px" }} aria-hidden="true">
+              <input type="text" name="b_dcb0fe74937e55e28c5843b7a_8fa0cc2c6e" tabindex="-1" value="" />
+            </div>
+            <div className="clear">
+              <button type="submit" name="subscribe" id="mc-embedded-subscribe" className="button">
+                <div className="btn-content">
+                  <p>Go</p>
+                </div>
+              </button>
+
+            </div>
           </div>
-          <button id="mc-embedded-subscribe" className="" type="submit" > 
-          <div className="btn-content">
-            <p>Go</p>
-            <img src="https://img.icons8.com/ios/28/000000/starred-ticket.png" alt="" />
-          </div>
-          </button>
         </form>
       </div>
-      </div>
 
 
-    <footer>
-      <div className="footer">
-        <a style={{ textDecoration: "none" }} href="https://www.instagram.com/whathecurl" target="_blank" rel="noopener noreferrer">@whathecurl</a>
-        <img className="heart" src={Like} alt="" />
-      </div>
-    </footer>
-                  {/* <img style={{width: "200px"}} src={Minhoca} alt="" /> */ }
-                </div >
-              
+
+
+      <footer>
+        <div className="footer">
+          <a style={{ textDecoration: "none" }} href="https://www.instagram.com/whathecurl" target="_blank" rel="noopener noreferrer">@whathecurl</a>
+          <img className="heart" src={Like} alt="" />
+        </div>
+      </footer>
+      {/* <img style={{width: "200px"}} src={Minhoca} alt="" /> */}
+    </div >
+
   );
 }
 
