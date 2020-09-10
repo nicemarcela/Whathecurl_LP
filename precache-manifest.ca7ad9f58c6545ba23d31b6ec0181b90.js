854abcbadca9e7085c020450332e29ce4708d982
nicemarcela <58824632+nicemarcela@users.noreply.github.com>
2020-09-10 17:23:45 +0200

    Updates

---
+++ precache-manifest.ca7ad9f58c6545ba23d31b6ec0181b90.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d818a7617454daa687f08169c8bd254f",
+    "url": "/Whathecurl_LP/index.html"
+  },
+  {
+    "revision": "439b948c009261b456ce",
+    "url": "/Whathecurl_LP/static/css/main.8aa57278.chunk.css"
+  },
+  {
+    "revision": "ecb308e06ee04a78bd6c",
+    "url": "/Whathecurl_LP/static/js/2.f22156f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Whathecurl_LP/static/js/2.f22156f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "439b948c009261b456ce",
+    "url": "/Whathecurl_LP/static/js/main.8cf1a4ea.chunk.js"
+  },
+  {
+    "revision": "1350f5c4d18d3092322e",
+    "url": "/Whathecurl_LP/static/js/runtime-main.843cabb6.js"
+  },
+  {
+    "revision": "8e71c8641761288f01d978b145a903ac",
+    "url": "/Whathecurl_LP/static/media/curl.8e71c864.png"
+  },
+  {
+    "revision": "f50837b169ccfa014c07fd16710cb85d",
+    "url": "/Whathecurl_LP/static/media/gradient-waves.f50837b1.png"
+  },
+  {
+    "revision": "43cd7cc4b86a3ed7e7b4360a4333b9f9",
+    "url": "/Whathecurl_LP/static/media/heart.43cd7cc4.png"
+  },
+  {
+    "revision": "3adecfe7601b000b3ca9bc1b79645e7e",
+    "url": "/Whathecurl_LP/static/media/lightpinkfolder1.3adecfe7.png"
+  },
+  {
+    "revision": "c30d59d900b4ca239b942d078c459e40",
+    "url": "/Whathecurl_LP/static/media/purplepolish.c30d59d9.png"
+  }
+]);
